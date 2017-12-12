36595074569712ab852122143ae11ad99754c5da
etorwas <torbjorn.wasterlid@ericsson.com>
2017-12-12 23:25:02 +0100

    part 1 day 2 solved

--- 2017/day2/part1.hs
+++ 2017/day2/part1.hs
@@ -1,7 +1,12 @@
 import Data.List.Split
 
+stringToInt :: [String] -> [Int]
+stringToInt xs = map read xs :: [Int]
+
+checkSum :: [Int] -> Int
+checkSum xs = maximum xs - minimum xs
+
 main :: IO ()
 main = do
   contents <- getContents
-  print $ length contents
-  print $ map (splitOn "\t") (lines contents)
+  print $ sum (map checkSum (map stringToInt (map (splitOn "\t") (lines contents))))
